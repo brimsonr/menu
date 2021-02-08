e985991d59d7b410739950e7be17910316e78594
brimsonr <78764009+brimsonr@users.noreply.github.com>
2021-02-09 01:29:10 +0200

    Create lyrics.js

---
+++ lyrics.js
@@ -0,0 +1,10 @@
+Window.loadDoc = function(url) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState === '4' && this.status === '200') {
+      document.body.innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
