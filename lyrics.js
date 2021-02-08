1626e2edecc06dc37c66fa027b071735cf6bd2a1
brimsonr <78764009+brimsonr@users.noreply.github.com>
2021-02-09 01:30:22 +0200

    Update lyrics.js

--- lyrics.js
+++ lyrics.js
@@ -1,4 +1,4 @@
-Window.loadDoc = function(url) {
+Window.loadDoc = function('lyrics.html') {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState === '4' && this.status === '200') {
