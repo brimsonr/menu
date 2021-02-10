44850ae93f462111ec0dbee4d1ffdd2bca236365
brimsonr <78764009+brimsonr@users.noreply.github.com>
2021-02-10 08:41:42 +0200

    Update lyrics.js

--- lyrics.js
+++ lyrics.js
@@ -1,10 +1,10 @@
-Window.loadDoc = function('lyrics.html') {
+Window.loadPlaylist = function('list.html') {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState === '4' && this.status === '200') {
       document.body.innerHTML = this.responseText;
     }
-  };
+  }
   xhttp.open("GET", url, true);
   xhttp.send();
 }
